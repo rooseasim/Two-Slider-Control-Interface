ea5ece9e4e906909e04d86e7c61968231bd57171
rooseasim <rooseasim@gmail.com>
2025-08-09 00:27:24 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sliders with Save</title>
+</head>
+<body>
+
+
+  <h2>Adjust the Sliders</h2>
+
+  <form action="sliderhtml.php" method="post">
+
+
+    <label>Slider 1: 
+      <input type="range" id=slider1 name="slider1" min="0" max="100" value="50">
+    </label>
+    <br><br>
+
+    <label>Slider 2: 
+      <input type="range" id=slider1 name="slider2" min="0" max="100" value="30">
+    </label>
+    <br><br>
+
+   
+
+    <button type="submit">Save</button>
+  </form>
+
+</body>
+</html>
